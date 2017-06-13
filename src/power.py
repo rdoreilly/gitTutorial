f149a2a1ee1a3193f16698588a7c9a06a38f294c
rdoreilly <ruairi.oreilly@cit.ie>
2017-06-13 12:19:05 +0100

    Trying to mimic conflicts - altering the same content locally and remotely

--- src/power.py
+++ src/power.py
@@ -1,8 +1,12 @@
+""" This is a script that calculates one number to  
+    power of another
+"""
+
 def to_the_power(x,y=2):
     result=x
     for i in range(0,y):
         result = result * x
     return result
 x = 2
-y = 5
+y = 7
 print ("{0} to the power of {1} is: ".format(x,y,to_the_power(x,y)))
