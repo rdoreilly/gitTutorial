bebd05dcf7991581e6345321d027f357bd2902d5
Ruairi O'Reilly <ruairi.oreilly@cit.ie>
2017-06-13 12:26:17 +0100

    changes made

--- src/power.py
+++ src/power.py
@@ -3,7 +3,7 @@ def to_the_power(x,y=2):
     for i in range(0,y):
         result = result * x
     return result
-x = 6 #
-y = 5
+x = 7
+y = 6 #
 print ("{0} to the power of {1} is: ".format(x,y,to_the_power(x,y)))
 print ("The End")
