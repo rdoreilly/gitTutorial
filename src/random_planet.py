3d2382b9b8d4f060613a4d9c9a3f5407ef4d9497
Ruairi O'Reilly <ruairi.oreilly@cit.ie>
2017-06-13 15:40:16 +0100

    Random planet script added and gitignore

---
+++ src/random_planet.py
@@ -0,0 +1,4 @@
+import random
+
+planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+print("One of the planets in our solar system is {0}".format(random.choice(planets)))
