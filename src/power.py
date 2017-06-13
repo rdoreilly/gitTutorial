a89bce8ff0e0af9b585426e154bfb2f545707bec
Ruairi O'Reilly <ruairi.oreilly@cit.ie>
2017-06-13 13:10:51 +0100

    changed how the power is calculated.

--- src/power.py
+++ src/power.py
@@ -2,14 +2,22 @@
     power of another
 """
 
-def to_the_power(x,y=2):
-    result=x
-    for i in range(0,y):
-        result = result * x
-    return result
+def to_the_power(x,y):
+#    result = x
+#    for i in range(1,y):
+#        result = result * x
+    return x**y
 
 x = 2
 y = 5 #
+result = to_the_power(x,y)
+print ("This birthday I will be {0} to the power of {1} which makes me: {2}".format(x,y,result))
 
-print ("{0} to the power of {1} is: ".format(x,y,to_the_power(x,y)))
+x = 2
+y = 6 #
+
+print ("Next year I will be: {2}".format(x,y,(to_the_power(x,y) - (result - 1))))
+
+
+
 print ("The End")
